95938a7b374af6f1de275dbbd0e82745f73a7b9b
Carlos Ramirez <c.ramirez@aqtiva.mx>
2020-08-05 21:38:17 -0500

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://cafeApp:FyC45VZJoBE9J6gr@cluster0-5xitk.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URL;
 }
 
 // creamos la variable URLDB de manera global
