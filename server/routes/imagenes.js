3153c4037e223f847d79a7b638a1a59f9a294f89
Carlos Ramirez <c.ramirez@aqtiva.mx>
2020-08-11 16:44:17 -0500

    Subida de archivos

---
+++ server/routes/imagenes.js
@@ -0,0 +1,31 @@
+const  express = require('express');
+let app = express();
+const fs = require('fs');
+const path = require('path');
+const {verificaTokenImg} = require('../middlewares/autenticacion')
+
+
+
+app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res)=>{
+
+    let tipo = req.params.tipo;
+    let img =  req.params.img;
+
+
+
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${img}`);
+
+    if (fs.existsSync(pathImagen)) {
+        res.sendFile(pathImagen);
+    }else{
+        let noImagePath = path.resolve(__dirname, '../assets/not-found.png');
+        res.sendfile(noImagePath);
+    }
+
+    
+    
+})
+
+
+
+module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,8 @@ app.use( require('./login'));
 app.use(require('./categoria'));
 // Importamos las rutas de productos
 app.use(require('./producto'));
-
-
+// Importamos las rutas para subir archivo
+app.use(require('./upload'));
+// Importamos las rutas para mostrar las imagenes
+app.use(require('./imagenes'));
 module.exports = app;
